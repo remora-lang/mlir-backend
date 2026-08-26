6fff3d67e15a4e3543d5aaecd2a2a7b7848a80ce
Nikolaj Hey Hinnerskov <nhey@900901.xyz>
2026-08-26 16:22:07 +0200

    Ignore blackbox(dummy).

--- mlir-backend/blackbox.hpp
+++ mlir-backend/blackbox.hpp
@@ -29,6 +29,8 @@ inline std::optional<BlackBox> MaybeBlackBox(const Attrs &attrs) {
     if (auto *a = std::get_if<CompAttr>(&attr.v)) {
       if (a->name == "blackbox" && a->args.size() == 1) {
         if (auto *v = std::get_if<AtomAttr>(&a->args[0].v)) {
+          if (v->name == "dummy")
+            continue;
           return ToBlackBox(v->name);
         }
       }
@@ -38,9 +40,5 @@ inline std::optional<BlackBox> MaybeBlackBox(const Attrs &attrs) {
 }
 
 inline bool IsBlackBox(const Attrs &attrs) {
-  for (const auto &attr : attrs.attrs)
-    if (auto *a = std::get_if<CompAttr>(&attr.v))
-      if (a->name == "blackbox" && a->args.size() == 1)
-        return true;
-  return false;
+  return MaybeBlackBox(attrs).has_value();
 }
